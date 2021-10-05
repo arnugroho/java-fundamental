d3fc1d9a51745cbf1bb2be307eed07cacb9b4168
arnugroho <nugroho.akbar.r@gmail.com>
2021-10-05 12:56:59 +0700

    contoh java structure

---
+++ src/com/arnugroho/j1_structure/JavaStructureExample.java
@@ -0,0 +1,37 @@
+
+
+//1. Documentation Section
+/*
+ * @author Akbar Riyan Nugroho
+ * @Created 5-10-2021
+ * @Description : Contoh Java Structure
+ */
+
+//2. Package Declaration
+package com.arnugroho.j1_structure;
+
+//3. Import Statement
+import java.util.Date;
+
+//4. Class Declaration
+public class JavaStructureExample {
+    //5. Field/Global Variable
+    public String sayHello = "Hello World";
+
+    //6. Method/Function
+    public void printWelcome(String name){
+        System.out.println(this.sayHello + " : " + name);
+    }
+
+    //6. Method/Function
+    public Date getCurrentdate(){
+        return new Date();
+    }
+
+    //6. Method/Function
+    public static void main(String[] args) {
+        JavaStructureExample example = new JavaStructureExample();
+        example.printWelcome("Akba Riyan Nugroho");
+        System.out.println("Current Time : " + example.getCurrentdate());
+    }
+}
