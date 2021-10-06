902fc8dff2f6db8250fca5e4d34a3da6982256c8
arnugroho <nugroho.akbar.r@gmail.com>
2021-10-06 19:53:50 +0700

    penambahan contoh kondisi

--- src/com/arnugroho/j3_materi_conditional/OperatorIf.java
+++ src/com/arnugroho/j3_materi_conditional/OperatorIf.java
@@ -10,17 +10,31 @@ import java.util.Scanner;
 
 public class OperatorIf {
     public static void main(String[] args) {
+        System.out.println("Masukkan Nilai a : ");
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt(); /*nilai a kita inisialisasi dengan nilai hasil inputan di console*/
         scan.close();
-        /*Apakah a bernilai lebih dari 4
-        * Jika ya, maka akan mencetak Kondisi a Sesuai
+        /*Apakah a bernilai lebih dari 2
+        * Jika ya, maka akan mencetak Kondisi 1 Sesuai
         * Jika tidak, maka tidak akan mencetak apapun*/
         /*
             {1} Operator If
         */
-        if (a > 4) {
-            System.out.println("Kondisi a Sesuai");
+        if (a > 2) {
+            System.out.println("Kondisi 1 Sesuai");
         }
+
+        /*Apakah a bernilai lebih dari 4 dan kurang dari 9
+         * Jika ya, maka akan mencetak Kondisi 2 Sesuai
+         * Jika tidak, maka tidak akan mencetak apapun*/
+        if (a > 4 && a < 9) {
+            System.out.println("Kondisi 2 Sesuai");
+        }
+
+        /*Test Input, Masukkan angka :
+        * 3
+        * 5
+        * 2
+        * */
     }
 }
