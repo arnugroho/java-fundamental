96b13b2d7c4cf83bf808b479d4ef46e56eee8286
arnugroho <nugroho.akbar.r@gmail.com>
2021-10-06 19:48:16 +0700

    update java structure

--- src/com/arnugroho/j1_materi_structure/JavaStructureExample.java
+++ src/com/arnugroho/j1_materi_structure/JavaStructureExample.java
@@ -1,34 +1,34 @@
 
 
-//1. Documentation Section
+//{1}. Documentation Section
 /*
  * @author Akbar Riyan Nugroho
  * @Created 5-10-2021
  * @Description : Contoh Java Structure
  */
 
-//2. Package Declaration
-package com.arnugroho.j1_structure;
+//{2}. Package Declaration
+package com.arnugroho.j1_materi_structure;
 
-//3. Import Statement
+//{3}. Import Statement
 import java.util.Date;
 
-//4. Class Declaration
+//{4}. Class Declaration
 public class JavaStructureExample {
-    //5. Field/Global Variable
+    //{5}. Field/Global Variable
     public String sayHello = "Hello World";
 
-    //6. Method/Function
+    //{6}. Method/Function
     public void printWelcome(String name){
         System.out.println(this.sayHello + " : " + name);
     }
 
-    //6. Method/Function
+    //{6}. Method/Function
     public Date getCurrentdate(){
         return new Date();
     }
 
-    //6. Method/Function
+    //{6}. Method/Function
     public static void main(String[] args) {
         JavaStructureExample example = new JavaStructureExample();
         example.printWelcome("Akba Riyan Nugroho");
