dd05ccd750fc4b2907fc077e90277afe41477601
arnugroho <nugroho.akbar.r@gmail.com>
2021-10-07 09:30:01 +0700

    no5

--- src/com/arnugroho/j2_latihan01/Lat5DeretHurufAngka.java
+++ src/com/arnugroho/j2_latihan01/Lat5DeretHurufAngka.java
@@ -14,7 +14,7 @@ package com.arnugroho.j2_latihan01;
 * jika n = 8
 * hasil : 2   A   4   B   6   C   8   D
 * */
-public class Lat4DeretHurufAngka {
+public class Lat5DeretHurufAngka {
     public static void main(String[] args) {
         /*Gunakan Scanner agar nilai n dinamis*/
     }
