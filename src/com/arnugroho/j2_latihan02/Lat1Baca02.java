e9603ec775ee9c5f1f82e4839cb21248614a4dfd
arnugroho <nugroho.akbar.r@gmail.com>
2021-10-09 10:32:13 +0700

    latihan baca 02

---
+++ src/com/arnugroho/j2_latihan02/Lat1Baca02.java
@@ -0,0 +1,24 @@
+/*
+ * @author Akbar Riyan Nugroho
+ * @Created 9-10-2021
+ *
+ */
+
+package com.arnugroho.j2_latihan02;
+
+/*
+* Membuat baris code untuk membaca inputan berupa kalimat
+* 1. Ubah huruf vokal menjadi angka berurutan
+*   contoh : akbar riyan nugroho menjadi 1kb2r r3y4n n5gr6h7
+* 2. Ubah huruf ditengah kata menjadi bintang
+*   contoh : akbar riyan nugroho menjadi a***r r***n n*****o
+* 3. ubah/memastikan huruf depan kata dalam suatu kalimat adalah huruf besar, sisanya huruf kecil
+*   contoh : akBar riyAn Nugroho menjadi Akbar Riyan Nugroho
+* 4. membalikkan urutan kata suatu kalimat
+*   contoh : akbar riyan nugroho menjadi nugroho riyan akbar
+* */
+public class Lat1Baca02 {
+    public static void main(String[] args) {
+        /*Gunakan Scanner agar kalimat dinamis*/
+    }
+}
