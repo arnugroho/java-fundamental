cf6a2cc02c2d8e064102748f72d7b297bcf28d23
arnugroho <nugroho.akbar.r@gmail.com>
2021-10-09 10:24:43 +0700

    latihan baca 01

---
+++ src/com/arnugroho/j2_latihan02/Lat1Baca01.java
@@ -0,0 +1,19 @@
+/*
+ * @author Akbar Riyan Nugroho
+ * @Created 9-10-2021
+ *
+ */
+
+package com.arnugroho.j2_latihan02;
+
+/*
+* Membuat baris code untuk membaca inputan berupa kalimat
+* 1. Hitung jumlah huruf dalam kalimat tersebut
+* 2. Hitung jumlah kata dalam kalimat tersebut
+* 3. Hitung jumlah huruf vokal dalam kalimat tersebut
+* */
+public class Lat1Baca01 {
+    public static void main(String[] args) {
+        /*Gunakan Scanner agar kalimat dinamis*/
+    }
+}
